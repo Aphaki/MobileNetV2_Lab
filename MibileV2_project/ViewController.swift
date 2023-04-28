087423c2b24289889fd4d3659ea6409eba956169
Aphaki <kiapha1108@gmail.com>
2023-04-28 17:02:51 +0900

    Initial Commit

---
+++ MibileV2_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MibileV2_project
+//
+//  Created by Sy Lee on 2023/04/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
